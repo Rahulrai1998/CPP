e975732e35cf839a9a44a92f3bee48a044b9c091
Rahul Kumar <rahulrai2822march@gmail.com>
2021-05-28 18:42:41 +0530

    ds - circular LL updated

--- dataStructures/Linkedlist/circularLL.cpp
+++ dataStructures/Linkedlist/circularLL.cpp
@@ -16,12 +16,16 @@ void create(node**, int);
 void display(node*);
 void insertAtHead(node** , int);
 void insertAtTail(node**, int);
+void insertAtPosition(node** , int , int);
 int deleteHead(node**);
 int deleteTail(node**);
 void reverseList(node**);
+int deletion(node** , int);
+void reverse(node**);
 
 int main()
 {
+	int deleted ; 
 	node *head = NULL ; 
 	create(&head , 5);
 	display(head);
@@ -29,6 +33,20 @@ int main()
 	display(head);	
 	insertAtTail(&head , 100);
 	display(head);	
+	deleted = deleteHead(&head);
+	if(deleted!=-9999)
+		cout << deleted << " is deleted" <<endl ; 
+	display(head);
+	deleted = deleteTail(&head);
+	if(deleted!=-9999)
+		cout << deleted << " is deleted"<<endl ; 
+	display(head);
+	deleted = deletion(&head , 3) ;
+	if(deleted!= -9999)
+		cout << deleted << " is deleted" <<endl ; 
+	 display(head) ; 
+	insertAtPosition(&head , 321 , 2);
+	display(head);
 	return 0 ;
 }
 void create(node**head , int n)
@@ -106,4 +124,150 @@ void insertAtTail(node**head , int item)
 	temp->next = newnode ; 
 	newnode->next = (*head) ; 
 	return ; 
+}
+int deleteHead(node**head)
+{
+	if((*head)==NULL)
+	{
+		cout <<"List is empty"<<endl ;
+		return -9999 ;  
+	}
+	node *temp = (*head) ;
+	node *temp2 = (*head) ; 
+	while(temp->next!=(*head))
+	{
+		temp = temp->next ; 	
+	}
+	int item = (*head)->info ; 
+	if(temp==*head)
+		*head = NULL ; 
+	else
+	{
+		*head = (*head)->next ; 
+		temp->next = (*head) ; 
+	}
+	temp2->next = NULL ; 
+	delete temp2 ; 
+	return item ; 
+}
+int deleteTail(node**head)
+{
+	if(*head ==NULL)
+	{
+		cout <<"List is empty"<<endl ;
+		return -9999 ; 
+		 
+	}
+	node *pre , *cur ; 
+	pre = NULL ; 
+	cur = (*head ); 
+	int item;
+	while(cur->next!=(*head))
+	{
+		pre = cur ; 
+		cur = cur->next ; 
+	}
+	item = cur->info ; 
+	if(cur == (*head)) 				
+		*head = NULL; 
+	else
+		pre->next = *head; 
+	cur->next = NULL ; 
+	delete cur ; 
+	return item ; 
+}
+int count(node *head)
+{
+	node *temp  = head; 
+	int nodeCount = 1; 
+	while(temp->next!=head)
+	{
+		temp = temp->next ;
+		nodeCount++ ; 
+		 
+	}
+	return (nodeCount);
+}
+int deletion(node**head , int pos)
+{
+	if(*head==NULL)
+	{
+		cout <<"List is empty"<< endl ; 
+		return -9999 ; 
+	}
+	if(pos>count(*head) || pos<=0 )
+	{
+		cout << "Invalid Position" << endl ;
+		return -9999; 
+	}
+	if(pos == 1)
+		return deleteHead(head);
+	node *pre , *toDelete; 
+	pre = NULL ; 
+	toDelete = *head ;
+	int cnt = 1 ;  
+	while(cnt != pos) 
+	{
+		pre = toDelete ; 
+		toDelete = toDelete->next ; 
+		cnt++ ; 
+	}
+	int item = toDelete->info ; 
+	pre->next = toDelete->next ; 
+	toDelete->next = NULL ; 
+	delete toDelete ; 
+	return item;
+}
+void insertAtPosition(node**head , int item , int pos)
+{
+	if(*head == NULL)
+	{
+		cout << "List is Empty " << endl ; 
+		return ; 
+	}
+	int totalNode = count(*head);
+	if(pos<=0 || pos > totalNode+1)
+	{
+		cout << "Invalid Position" <<endl ; 
+		return ; 
+	}
+	int cnt = 1 ;
+	node *newnode , *temp ; 
+	temp = *head ; 
+	while(cnt != pos - 1)
+	{
+		temp = temp->next ; 
+		cnt++ ; 
+	} 
+	newnode = new node(item) ; 
+	if(pos == 1)
+	{
+		node *loc = *head ; 
+		while(loc->next!=*head)
+		{
+			loc = loc->next ; 
+		}
+		loc->next = newnode ; 
+		newnode->next = *head ; 
+		*head = newnode ; 
+	}
+	else
+	{
+		newnode->next = temp->next ; 
+		temp->next = newnode ; 
+	}
+	return ; 
+}
+void reverse()
+{
+	if(*head==NULL)
+	{
+		cout <<"List is empty"<<endl ;
+		return ;  
+	}
+	
+
+
+
+	return ;
 }
